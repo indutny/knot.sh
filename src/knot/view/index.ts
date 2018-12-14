1e04a5a3c3594cbc55bef32b4d6ffd391dd2a445
Fedor Indutny <fedor@indutny.com>
2018-12-14 14:22:06 -0500

    knot: fixes

--- src/knot/view/index.ts
+++ src/knot/view/index.ts
@@ -1,4 +1,4 @@
-export { IViewFrame, IWindowSize, View, ViewEvent } from './base';
+export { View, ViewEvent } from './base';
 export { Editor } from './editor';
 export { Prompt } from './prompt';
 export { Window } from './window';
